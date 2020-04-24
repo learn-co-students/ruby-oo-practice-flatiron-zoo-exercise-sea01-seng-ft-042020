7788ca8ef628c4f125ac33dc2729574a22c28b9f
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-04-23 17:04:58 -0700

    lab completed

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,22 @@
 class Animal
+    attr_accessor :weight, :zoo
+    attr_reader :species, :nickname
+
+    @@all = []
+
+    def initialize(species, weight, nickname)
+        @species = species
+        self.weight = weight
+        @nickname = nickname
+        self.class.all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(species)
+        self.all.select {|animal| animal.species == species}
+    end
 
 end
--- run.rb
+++ run.rb
@@ -2,9 +2,14 @@ require_relative "lib/Animal.rb"
 require_relative "lib/Zoo.rb"
 require 'pry'
 
+bob = Animal.new("Dog",35,"Bob")
+todd = Animal.new("Dog",30,"Todd")
+jim = Animal.new("Cat",10,"Jim")
 
-#Test your code here
+seattle_zoo = Zoo.new("Seattle City Zoo","Seattle, WA")
 
+todd.zoo = seattle_zoo
+bob.zoo = seattle_zoo
 
 binding.pry
 puts "done"
