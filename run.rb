b20f769f8c2e15d832a478713618579b0fa6c507
ckaiser258 <kaiserc2@seattleu.edu>
2020-04-26 21:01:02 -0700

    Done.

--- run.rb
+++ run.rb
@@ -3,8 +3,13 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+colton = Zoo.new("colton", "chicago")
+christina = Zoo.new("christina", "chicago")
+joe = Zoo.new("joe", "phoenix")
 
-
+chica = Animal.new("dog", 20, "chica", "colton")
+snoopy = Animal.new("dog", 50, "snoopy", "colton")
+meow = Animal.new("cat", 5, "meow", "colton")
+scyther = Animal.new("snake", 10, "scyther", "christina")
 binding.pry
 puts "done"
