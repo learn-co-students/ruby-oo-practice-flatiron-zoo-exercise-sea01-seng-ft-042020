0ef6a398574c435993ad78ed3e1d2c46e6d3ce36
matthewsedlacek <sedlacek1991@gmail.com>
2020-04-28 21:40:45 -0700

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Zoo
+    attr_accessor :name, :location
+
+
+    @@all = []
+
+    def initialize(name,location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def animals
+        Animal.all.select {|animal| animal.zoo == self}
+    end
+
+    def animal_species
+       all_species = animals.map {|animal|animal.species}
+       all_species.uniq
+    end
+
+    def find_by_species(species)
+        Animal.all.select {|animal|animal.species == species}
+    end
+
+    def nicknames
+        animals.map {|animal|animal.nickname}
+    end
+
+    def self.find_by_location(location)
+        self.all.select{|zoo|zoo.location == location}
+    end
 
 end
